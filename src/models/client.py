39ddd9c43d5222e47183f8ef3b0ff32446fb1a04
Yesid Perea <yesidp@gmlsoftware.com>
2020-07-16 07:47:33 -0500

    Modifying client.py module to support SQL Alchemy integration.

--- src/models/client.py
+++ src/models/client.py
@@ -4,17 +4,23 @@
     Este modulo contiene la clase ClientModel la cual aloja y maneja 
     las operaciones contra la entidad 'Clientes' en la base de datos
 """
+from db import db
 
-class ClientModel():
+
+class ClientModel(db.Model):
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(500))
+
     """
     La clase ClientModel almacena y maneja las operaciones 
     contra la entidad 'Clientes' en la base de datos
     """
-    def __init__(self, _id: int, name: str, description: str):
+    def __init__(self, name: str, description: str):
         """
         Inicializa la clase Client con la informacion basica del cliente.
         """
-        self.id = _id
         self.name = name
         self.description = description
 
@@ -23,3 +29,19 @@ class ClientModel():
         Serializa el objeto ClientModel en formato json.
         """
         return {'id': self.id, 'name': self.name, 'description': self.description}
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
