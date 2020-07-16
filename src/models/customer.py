65416f9df475e85beec4b3a1074e18c5f0a9e7fe
Yesid Perea <yesidp@gmlsoftware.com>
2020-07-16 07:49:46 -0500

    Renaming client.py module for customer.py

--- src/models/customer.py
+++ src/models/customer.py
@@ -1,20 +1,20 @@
 """
     client.py
     -------------
-    Este modulo contiene la clase ClientModel la cual aloja y maneja 
+    Este modulo contiene la clase CustomerModel la cual aloja y maneja 
     las operaciones contra la entidad 'Clientes' en la base de datos
 """
 from db import db
 
 
-class ClientModel(db.Model):
+class CustomerModel(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     description = db.Column(db.String(500))
 
     """
-    La clase ClientModel almacena y maneja las operaciones 
+    La clase CustomerModel almacena y maneja las operaciones 
     contra la entidad 'Clientes' en la base de datos
     """
     def __init__(self, name: str, description: str):
@@ -26,7 +26,7 @@ class ClientModel(db.Model):
 
     def json(self):
         """
-        Serializa el objeto ClientModel en formato json.
+        Serializa el objeto CustomerModel en formato json.
         """
         return {'id': self.id, 'name': self.name, 'description': self.description}
 
